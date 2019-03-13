31ef2b48e26d8ff96fcd2d3135f40ec59f8c8c22
aniekanedet <anieaedet@gmail.com>
2019-03-13 16:38:02 +0100

    update files

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-// $(document).ready(function() {
-//   setTimeout(function() {
-//     $("body").addClass("loaded");
-//     $("h1").css("color", "#222222");
-//   }, 22);
-// });
-
-$(window).load(function() {
-	// Animate loader off screen
-	$(".se-pre-con").fadeOut("slow");;
+$(document).ready(function() {
+  setTimeout(function() {
+    $("body").addClass("loaded");
+    $("h1").css("color", "#222222");
+  }, 22);
 });
+
+// $(window).load(function() {
+// 	// Animate loader off screen
+// 	$(".se-pre-con").fadeOut("fast");;
+// });
